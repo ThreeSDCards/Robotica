25a4acdc5cd221a546e461308a41ff2e953bedb7
Tobias Draisma <tobiastijmen@gmail.com>
2020-04-18 01:50:04 +0200

    Pen header verbeterd

--- Model/Pen.h
+++ Model/Pen.h
@@ -1,11 +1,13 @@
-#ifndef PEN_H
-#define PEN_H
+#pragma once
+#include "Square.h"
+#include "PivotPoint.h"
+#include "ServoDriver.h"
 
-class Pen : Square {
+class Pen : Square
+{
 
 private:
-	PivotPoint* PivotX,_PivotY;
-	ServoDriver* servo;
+	PivotPoint *PivotX, *PivotY;
+	ServoDriver *servo;
+	Pen(PivotPoint *X, PivotPoint *Y, ServoDriver *servo);
 };
-
-#endif
