b9cc56e2e3783c9c32e4a2d35bc4d0074d2d7a8f
Tobias Draisma <tobiastijmen@gmail.com>
2020-04-18 16:45:07 +0200

    Test toegevoegd

--- Model/Point.cpp
+++ Model/Point.cpp
@@ -7,3 +7,20 @@ Point Point::Add(float x, float y)
 {
 	return Point(X + x, Y + y);
 }
+
+bool test_Point()
+{
+	try
+	{
+		Point p(3, 0);
+
+		//Test Add
+		if (p.Add(5, 4).X != 8 || p.Add(0, -2).Y != -2)
+			throw "POINT ERROR (ADD)";
+		return true;
+	}
+	catch (const char *e)
+	{
+		return false;
+	}
+}
