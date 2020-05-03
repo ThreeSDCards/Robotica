c3fdcad73ec3762a3649bfaf8c32c2b2eb5c9323
Tobias Draisma <tobiastijmen@gmail.com>
2020-05-03 14:16:06 +0200

    Time tests toegevoegd

--- Model/Time.cpp
+++ Model/Time.cpp
@@ -6,8 +6,26 @@ void Time::Update()
 
     //Calculate ms elapsed
     auto elapsed = now - lastTime;
-    DeltaTime = elapsed.count;
+    DeltaTime = elapsed.count();
 
     //Update last known time
     lastTime = now;
+}
+
+bool test_Time()
+{
+    try
+    {
+        using namespace std::chrono_literals;
+        Time t;
+        std::this_thread::sleep_for(1s);
+        t.Update();
+        if (t.DeltaTime > 1005 || t.DeltaTime < 995)
+            throw "TIME ERROR";
+    }
+    catch (const char *e)
+    {
+        return false;
+    }
+    return true;
 }
--- Model/Time.h
+++ Model/Time.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <chrono>
+#include <thread>
 
 class Time
 {
     std::chrono::_V2::system_clock::time_point lastTime;
 
 public:
-    Time() : lastTime(chrono::high_resolution_clock::now()){};
+    Time() : lastTime(std::chrono::high_resolution_clock::now()){};
     float DeltaTime;
     void Update();
 };
+
+bool test_Time();
