95eddca25cb6174c7e30feb24c40e23786640b43
Tobias Draisma <tobiastijmen@gmail.com>
2020-05-14 13:56:46 +0200

    Implimented View object

--- View/View.cpp
+++ View/View.cpp
@@ -1,11 +1,29 @@
 #include "View.h"
 
-void View::Init() {
-	// TODO - implement View::Init
-	throw "Not yet implemented";
+View::View(Model *m)
+    : model(m), t(Time()) {}
+
+void View::Update()
+{
+    //New frame, so set timer
+    t.Update();
+    //Get collection of dynamic modules
+    auto modules = model->getDynamicObjects();
+    //Update all of their states
+    for (auto module : modules)
+    {
+        module->Routine(t.DeltaTime);
+    }
 }
 
-void View::Update() {
-	// TODO - implement View::Update
-	throw "Not yet implemented";
+void View::Draw()
+{
+    //Get collection of all drawable shapes
+    auto shapes = model->getDrawables();
+
+    //Call Draw() on all of them
+    for (auto shape : shapes)
+    {
+        shape->Draw();
+    }
 }
--- View/View.h
+++ View/View.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "../Model/Drawable.h"
 #include "../Model/Model.h"
+#include "../Model/Time.h"
+#include <vector>
 
 class View : public Drawable
 {
 private:
 	Model *model;
+	Time t;
 
 public:
 	View(Model *model);
 	void Update();
+	void Draw() override;
 };
