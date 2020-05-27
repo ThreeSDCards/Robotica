75e6b78ae166e168f7fe7422073d110fe4708e75
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-05-27 21:40:51 +0200

    added tasks

--- Model/ServoDriver.h
+++ Model/ServoDriver.h
@@ -1,15 +1,29 @@
 #pragma once
-class ServoDriver
+#include <queue>
+#include "Module.h"
+#include "Task.h"
+#include "Dynamic.h"
+#include <iostream>
+
+class ServoDriver : public Module, public Dynamic
 {
 
 private:
 	bool up = false;
+	std::queue<Task> tasks;
+	float timeRemaining = 1;
 
 public:
-	ServoDriver() : up(false){};
-	void Toggle();
+	ServoDriver() : up(false), tasks(std::queue<Task>()), timeRemaining(0) { Enable(); SetReady(true); };
 
 	bool IsUp();
+	void AddTask(Task task);
+	void AddTask(std::queue<Task> tasks);
+	void Routine(float) override;
+
+private:
+	void DoTask(Task task);
+	void Toggle();
 };
 
 bool test_ServoDriver();
