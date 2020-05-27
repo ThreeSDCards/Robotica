f48b59dc05a6e0b1383cb9caa9d6c593877bf2b3
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-05-27 21:43:36 +0200

    added draw functie

--- Model/Pen.h
+++ Model/Pen.h
@@ -2,15 +2,20 @@
 #include "Square.h"
 #include "ServoDriver.h"
 #include "Dynamic.h"
+#include <vector>
+#include <iostream>
 
 class Pen : public Square, public Dynamic
 {
-
 private:
+	int sometimes;
+	Point old;
 	Shape *PivotX, *PivotY;
 	ServoDriver *servo;
+	std::vector<Line> trace;
 
 public:
 	Pen(Shape *X, Shape *Y, ServoDriver *servo);
 	void Routine(float) override;
+	void Draw() override;
 };
