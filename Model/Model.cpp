db9765f6151cca27fbb80636aeb8878a538e8bc3
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-05-27 21:37:37 +0200

    added half a circle

--- Model/Model.cpp
+++ Model/Model.cpp
@@ -16,6 +16,7 @@ std::vector<Dynamic *> Model::getDynamicObjects()
 		out.push_back(XDriver);
 		out.push_back(YDriver);
 		out.push_back(pen);
+		out.push_back(servo);
 		return out;
 	}
 	else
@@ -27,36 +28,73 @@ std::vector<Dynamic *> Model::getDynamicObjects()
 
 void Model::Init()
 {
-	xPivot = new Square({-0.5f, 0.5f}, 0.05f);
-	yPivot = new Square({-0.5f, 0.5f}, 0.05f);
+	xPivot = new Square({ -0.5f, 0.5f }, 0.05f);
+	yPivot = new Square({ -0.5f, 0.5f }, 0.05f);
 	XDriver = new MotorDriver(*xPivot, true);
 	YDriver = new MotorDriver(*yPivot, false);
 
-	ServoDriver *ZDriver = new ServoDriver();
-	pen = new Pen(xPivot, yPivot, ZDriver);
+	servo = new ServoDriver();
+	pen = new Pen(xPivot, yPivot, servo);
 	Task task = Task();
 
-	//v0.0.2: Diamond
-	task.Time = 500;
-	task.Dest = 0.5f;
-	YDriver->AddTask(task);
+	//v0.0.3: Curves!
+	//X
 	task.Time = 1000;
-	task.Dest = -0.5f;
-	YDriver->AddTask(task);
-	task.Dest = 0.5f;
-	YDriver->AddTask(task);
-	//Add instructions to XDriver
-	task.Dest = 0.5f;
+	task.Dest = 0;
 	XDriver->AddTask(task);
 	task.Dest = -0.5f;
 	XDriver->AddTask(task);
-	task.Time = 500;
-	task.Dest = 0.0f;
+	task.Dest = 0;
 	XDriver->AddTask(task);
+	task.Dest = 0.5;
+	XDriver->AddTask(task);
+
+	//Y
+	task.Time = 1000;
+	task.Dest = 0.5f;
+	YDriver->AddTask(task); //Wait for X to get in position
+	YDriver->AddTask(MakeCurve(5, 1000, 0.5, 0.5, false,false));
+	YDriver->AddTask(MakeCurve(5, 1000, 0.5, 0.5, true,false));
+	YDriver->AddTask(MakeCurve(5, 1000, 0.5, 0.5, true, true));
+	//YDriver->AddTask(MakeCurve(5, 1000, 0.5, 0.5, false, true));
+
+	//Z
+	task.Time = 1000;
+	servo->AddTask(task);
+
 
 	initialized = true;
 }
 
+std::queue<Task> Model::MakeCurve(int def, float time, float XLength, float YLength, bool invertY, bool invertX) {
+	std::queue<Task> out = std::queue<Task>();
+	float timeChunk = time / def;
+	float XChunk = XLength / def;
+	Task task = Task();
+	task.Time = timeChunk;
+	for (int i = 0; i < def; i++) {
+		if (!invertY && !invertX) {
+			task.Dest = 1 - sin(acos(((i + 1) * XChunk) / XLength));
+			task.Dest *= YLength;
+			task.Dest = 1 - task.Dest;
+			task.Dest -= 0.5;
+			out.push(task);
+		}
+		else if(invertY && !invertX) {
+			task.Dest = 1 - sin(acos(((def - i - 1) * XChunk) / XLength));
+			task.Dest *= YLength;
+			task.Dest -= 0.5;
+			out.push(task);
+		}
+		else if (invertY && invertX) {
+			task.Dest = 1 - sin(acos(((def - i -1) * !XChunk) / XLength));
+			task.Dest *= YLength;
+			task.Dest -= 0.5;
+			out.push(task);
+		}
+	}
+	return out;
+}
 std::vector<Shape *> Model::getDrawables()
 {
 	if (initialized)
@@ -79,8 +117,9 @@ bool test_Model()
 	try
 	{
 	}
-	catch (const std::exception &e)
+	catch (const char* e)
 	{
+		printf("%s\n", e);
 		return false;
 	}
 	return true;
