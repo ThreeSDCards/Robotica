42cbb0af20fe21c786f8c888851eaa00b35e4deb
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-05-27 21:48:05 +0200

    added servo routine

--- Model/ServoDriver.cpp
+++ Model/ServoDriver.cpp
@@ -20,15 +20,61 @@ bool test_ServoDriver()
 		if (sd.IsUp())
 			throw "SERVODRIVER ERROR (ISUP)";
 
-		//Test Toggle
+		/*				OBSOLETE AFTER REINVENTING SERVODRIVER
 		sd.Toggle();
 		if (!sd.IsUp())
 			throw "SERVODRIVER ERROR (TOGGLE)";
-
+			*/
 		return true;
 	}
 	catch (const char *e)
 	{
 		return false;
 	}
+}
+
+void ServoDriver::AddTask(Task task) {
+	tasks.push(task);
+}
+
+void ServoDriver::AddTask(std::queue<Task> tasks) {
+	auto size = tasks.size();
+	for (int i = 0; i < size; i++) {
+		tasks.push(tasks.front());
+		tasks.pop();
+	}
+}
+
+void ServoDriver::Routine(float DeltaTime)
+{
+	if (Enabled()) {
+		//Execute current task
+		if (!IsReady())
+		{
+			//Update timekeeping
+			timeRemaining -= DeltaTime;
+
+			//Check if finished with task.
+			if (timeRemaining <= 0) {
+				Toggle();
+				SetReady(true);
+			}
+		}
+		//Check if there are any tasks in queue
+		if (IsReady() && !tasks.empty()) {
+			DoTask(tasks.front());
+			tasks.pop();
+		}
+
+		if (IsReady() && tasks.empty()) {
+			std::cout << "Driver Z" << ": DONE!\n";
+			Disable();
+		}
+	}
+}
+
+void ServoDriver::DoTask(Task task) {
+	//Set member fields
+	timeRemaining = task.Time;
+	Module::SetReady(false);
 }
