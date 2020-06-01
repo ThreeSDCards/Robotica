410ae6d433a569a2a0b740ec8f82b2eb8c491f7e
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-06-01 18:55:29 +0200

    point -> point2

--- Model/Shape.cpp
+++ Model/Shape.cpp
@@ -5,12 +5,12 @@ void Shape::Move(float x, float y)
 	SetOrig({GetOrig().X + x, GetOrig().Y + y});
 }
 
-void Shape::SetOrig(const Point &pos)
+void Shape::SetOrig(const Point2 &pos)
 {
 	orig = pos;
 }
 
-Point Shape::GetOrig() const
+Point2 Shape::GetOrig() const
 {
 	return orig;
 }
